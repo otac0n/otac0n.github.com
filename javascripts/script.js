97eea6d000f8acf6f777ed45e24081445f3d863d
John Gietzen <otac0n@gietzen.us>
2012-11-17 19:39:14 -0800

    Switched to google-code-prettify for syntax hilighting.

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,23 +1,6 @@
-(function($) {
-$(document).ready(function(){
-
-  // putting lines by the pre blocks
-  $("pre").each(function(){
-    var pre = $(this).text().split("\n");
-    var lines = new Array(pre.length+1);
-    for(var i = 0; i < pre.length; i++) {
-      var wrap = Math.floor(pre[i].split("").length / 70)
-      if (pre[i]==""&&i==pre.length-1) {
-        lines.splice(i, 1);
-      } else {
-        lines[i] = i+1;
-        for(var j = 0; j < wrap; j++) {
-          lines[i] += "\n";
-        }
-      }
-    }
-    $(this).before("<pre class='lines'>" + lines.join("\n") + "</pre>");
-  });
+$(function(){
+  $('pre').addClass('prettyprint');
+  prettyPrint();
 
   var headings = [];
 
@@ -49,4 +32,3 @@ $(document).ready(function(){
     return false;
   })
 });
-})(jQuery)
